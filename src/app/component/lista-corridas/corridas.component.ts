5f913e5decd7bc3ac2f98ef4471ba0ac28c77992
Gleidson <gleidsonrosasantos2@gmail.com>
2026-08-17 14:02:41 -0300

    agora corridas tbm exibe no console, de um jeio meio porco mas vai

--- src/app/component/lista-corridas/corridas.component.ts
+++ src/app/component/lista-corridas/corridas.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Routes, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { Atleta } from '../../models/atleta';
+import { AtletaService } from '../../servive/atleta.service';
+
+import { CadCorrida } from '../../models/cadastro-corridas';
+import { CadCorridaService } from '../../servive/cad-corrida.service';
+
+import { CadastroCorridasComponent } from '../cadastro-corridas/cadastro-corridas.component';
+import { AtletaComponent } from '../atleta-component/atleta.component';
 
 @Component({
   selector: 'app-corridas',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet, FormsModule],
   templateUrl: './corridas.component.html',
   styleUrl: './corridas.component.css'
 })
-export class CorridasComponent {
+export class CorridasComponent implements OnInit {
+
+  cadCorridas: CadCorrida[] = [];
+
+  constructor( private cadCorridaService: CadCorridaService ) {}
+
+  ngOnInit(): void {
+
+    console.log('corridas.component.ts carregado');
+
+    this.cadCorridas = this.cadCorridaService.listarCorrida();
+
+    console.table(this.cadCorridas);
+  }
 
 }
